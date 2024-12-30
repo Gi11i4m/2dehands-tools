64160dfa52c3d330b67d6ed802b61a75683dffb4
Gilliam <gi11i4m@gmail.com>
2024-12-31 00:21:11 +0100

    :test_tube: also run on deploy

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { bootstrap } from "@dx/inject";
 import { Server } from "./server.ts";
 
+async function run() {
+  await bootstrap(Server).run();
+}
+
 Deno.cron(
   "Vernieuw zoekertjes elke nacht",
   "0 0 * * *",
-  async () => await bootstrap(Server).run(),
+  async () => await run(),
 );
+
+await run();
