64168668a6762a209174ef2818190d8be02b9370
Gilliam <gi11i4m@gmail.com>
2025-09-25 09:11:57 +0200

    :bug: don't execute the loop immediately because the first function execution is not checking wether the system is unlocked or online

--- browser-extension/src/loop.mjs
+++ browser-extension/src/loop.mjs
@@ -1,14 +1,12 @@
 const ALARM_NAME = "2dehands-tools-alarm";
 
 /**
- * Executes a function immediately and then repeatedly at a specified interval, using the chrome.alarms API.
+ * Executes a function repeatedly at a specified interval, using the chrome.alarms API.
  *
  * @param {() => void} fn - The function to execute.
  * @param {number} periodInMinutes - The interval in minutes.
  */
 export async function loop(fn, periodInMinutes) {
-  fn();
-
   await chrome.alarms.clear(ALARM_NAME);
   await chrome.alarms.create(ALARM_NAME, { periodInMinutes });
 
